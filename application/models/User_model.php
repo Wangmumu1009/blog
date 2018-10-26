31705dbc05a68745c152d9b7ad7fca4e9b9fe122
Wangmumu1009 <18846128334@163.com>
2018-10-26 21:54:05 +0800

    blog

---
+++ application/models/User_model.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: asus-pc
+ * Date: 2018/10/20
+ * Time: 14:49
+ */
+
+class User_model extends CI_Model
+{
+    public function save($name,$password){
+        $data = array(
+            'name' => $name,
+            'password' => $password,
+        );
+        $query = $this->db->insert('t_user',$data);
+        return $query;
+    }
+
+    public function get_user_by_name_and_pwd($name,$password){
+
+        $query = $this->db->get_where('t_user',array(
+            'name' => $name,
+            'password' => $password,
+        ));
+        return $query->row();
+    }
+}
