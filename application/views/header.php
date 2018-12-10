eceb83480a75a1b4cb860d142cbcde9bfb096f96
Wangmumu1009 <18846128334@163.com>
2018-12-10 10:27:01 +0800

    blog

--- application/views/header.php
+++ application/views/header.php
@@ -11,11 +11,11 @@
 <div id="OSC_Topbar">
     <div id="VisitorInfo">
         当前访客身份：
-        <?php if (isset($user)){
+        <?php if(isset($user)){
             echo $user->name;
-            echo "[ <a href='welcome/logout'> 退出</a>]";
-        }else{ ?>
-        游客[<a href="welcome/login">登录</a> | <a href="welcome/reg">注册</a>]
+            echo "[ <a href='welcome/logout'>退出</a> ]";
+         }else{ ?>
+        游客 [ <a href="welcome/login">登录</a> | <a href="welcome/reg">注册</a> ]
         <?php }?>
 
 				<span id="OSC_Notification">
@@ -32,7 +32,7 @@
     <div class="clear"></div>
 </div>
 <div id="OSC_Content"><div class="SpaceChannel">
-        <div id="portrait"><a href="adminIndex.htm"><img src="images/portrait.gif" alt="Johnny" title="Johnny" class="SmallPortrait" user="154693" align="absmiddle"></a></div>
+        <div id="portrait"><a href="welcome/blog_list"><img src="images/portrait.gif" alt="Johnny" title="Johnny" class="SmallPortrait" user="154693" align="absmiddle"></a></div>
         <div id="lnks">
             <strong>Johnny的博客</strong>
             <div><a href="index_logined.htm">TA的博客列表</a>&nbsp;|
